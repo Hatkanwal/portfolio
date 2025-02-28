eb48f9aa7884fb8c15f0749db835fb51fee71d11
sohum r <164223586+heavensentsora@users.noreply.github.com>
2025-02-27 20:16:57 -0500

    chahges for github pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,13 +27,7 @@ export class AppComponent implements OnInit{
     this.languageService.initLanguage()
 
 
-    this.titleService.setTitle( "José Andrés | Frontend Developer" );
-
-    this.metaService.addTags([
-      {name: 'keywords', content: 'Frontend, software, developer'},
-      {name: 'description', content: 'Con 4 años de experiencia desarrollando sistemas, interfaces, bots y soluciones tecnológicas  para hacer de la web un lugar mejor. En mi trabajo me gusta liderar, proponer y ejecutar ideas, escribir y refactorizar código limpio, reutilizable y escalable.'},
-    ]);
-    
+    this.titleService.setTitle( "Mandeep Dhillon | Sprachmittler" );    
     
     AOS.init(); 
 
